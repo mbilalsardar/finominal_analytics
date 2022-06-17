e82ecccbe3d44b9150d2b0637e15fe0b1b2705b0
bilal <bilal@3ilogic.com>
2022-06-17 15:19:43 +0500

    added new dashboard link

--- dashboard.php
+++ dashboard.php
@@ -60,39 +60,6 @@ echo <<<'EOF'
 EOF;
 
 
-// function get_user_role($uid) {
-
-//     global $DB;
-
-//     $query = "SELECT
-//     u.id,
-//     u.username,
-//     r.shortname AS 'role',
-//     CASE ctx.contextlevel 
-//       WHEN 10 THEN 'system'
-//       WHEN 20 THEN 'personal'
-//       WHEN 30 THEN 'user'
-//       WHEN 40 THEN 'course_category'
-//       WHEN 50 THEN 'course'
-//       WHEN 60 THEN 'group'
-//       WHEN 70 THEN 'course_module'
-//       WHEN 80 THEN 'block'
-//      ELSE CONCAT('unknown context: ',ctx.contextlevel)
-//     END AS 'context_level',
-//     ctx.instanceid AS 'context_instance_id'
-//     FROM mdl_role_assignments ra
-//     JOIN mdl_user u ON u.id = ra.userid
-//     JOIN mdl_role r ON r.id = ra.roleid
-//     JOIN mdl_context ctx ON ctx.id = ra.contextid
-//     WHERE u.id=?
-//     GROUP BY u.id
-//     ORDER BY u.username ";
-
-//     $result=$DB->get_record_sql($query,[$uid]);
-
-//     return $result;
-// }
-
 
 $blocklink = new moodle_url('/blocks/finominal_analytics');
 $my = new moodle_url('/my');
@@ -100,19 +67,16 @@ $my = new moodle_url('/my');
 
 // $userroles = $this->get_user_role($USER->id);
 dd($userroles,0);
-// if($userroles->role == 'manager' || is_siteadmin()) {
-//     // $this->content->text = '<a style="font-size:18px;" href="'.$blocklink.'/team_dashboard.php"><i class="fa fa-tachometer" aria-hidden="true"></i>&nbsp'.get_string('teamdashboard',$this->blockname).'</a><br />';
-//     $url = new \moodle_url('/blocks/finominal_analytics/team_dashboard.php');
-//     echo $url;
-//     // redirect($url);
-// } 
-// elseif ($userroles->role == 'student') { 
-//     // $this->content->text .= '<a style="font-size:18px;" href="'.$blocklink.'/indi_dashboard.php"><i class="fa fa-tachometer" aria-hidden="true"></i>&nbsp'.get_string('individualdashboard',$this->blockname).'</a><br />';
-//     $url = new \moodle_url('/blocks/finominal_analytics/indi_dashboard.php');
-//     echo $url;
-
-//     // redirect($url);
-// }
+if($userroles->role == 'manager' || is_siteadmin()) {
+    // $this->content->text = '<a style="font-size:18px;" href="'.$blocklink.'/team_dashboard.php"><i class="fa fa-tachometer" aria-hidden="true"></i>&nbsp'.get_string('teamdashboard',$this->blockname).'</a><br />';
+    $url = new \moodle_url('/blocks/finominal_analytics/team_dashboard.php');
+    redirect($url);
+} 
+elseif ($userroles->role == 'student') { 
+    // $this->content->text .= '<a style="font-size:18px;" href="'.$blocklink.'/indi_dashboard.php"><i class="fa fa-tachometer" aria-hidden="true"></i>&nbsp'.get_string('individualdashboard',$this->blockname).'</a><br />';
+    $url = new \moodle_url('/blocks/finominal_analytics/indi_dashboard.php');
+    redirect($url);
+}
 
 
 echo $OUTPUT->footer();
