b2e4784f515a7aa19ebf939dd53fdccf7c722a6c
bilal <bilal@3ilogic.com>
2022-06-17 15:15:34 +0500

    added new dashboard link

--- dashboard.php
+++ dashboard.php
@@ -51,38 +51,38 @@ $returnurl = $baseurl;
 global $DB,$USER,$CFG;
 
 
-function get_user_role($uid) {
+// function get_user_role($uid) {
 
-    global $DB;
+//     global $DB;
 
-    $query = "SELECT
-    u.id,
-    u.username,
-    r.shortname AS 'role',
-    CASE ctx.contextlevel 
-      WHEN 10 THEN 'system'
-      WHEN 20 THEN 'personal'
-      WHEN 30 THEN 'user'
-      WHEN 40 THEN 'course_category'
-      WHEN 50 THEN 'course'
-      WHEN 60 THEN 'group'
-      WHEN 70 THEN 'course_module'
-      WHEN 80 THEN 'block'
-     ELSE CONCAT('unknown context: ',ctx.contextlevel)
-    END AS 'context_level',
-    ctx.instanceid AS 'context_instance_id'
-    FROM mdl_role_assignments ra
-    JOIN mdl_user u ON u.id = ra.userid
-    JOIN mdl_role r ON r.id = ra.roleid
-    JOIN mdl_context ctx ON ctx.id = ra.contextid
-    WHERE u.id=?
-    GROUP BY u.id
-    ORDER BY u.username ";
+//     $query = "SELECT
+//     u.id,
+//     u.username,
+//     r.shortname AS 'role',
+//     CASE ctx.contextlevel 
+//       WHEN 10 THEN 'system'
+//       WHEN 20 THEN 'personal'
+//       WHEN 30 THEN 'user'
+//       WHEN 40 THEN 'course_category'
+//       WHEN 50 THEN 'course'
+//       WHEN 60 THEN 'group'
+//       WHEN 70 THEN 'course_module'
+//       WHEN 80 THEN 'block'
+//      ELSE CONCAT('unknown context: ',ctx.contextlevel)
+//     END AS 'context_level',
+//     ctx.instanceid AS 'context_instance_id'
+//     FROM mdl_role_assignments ra
+//     JOIN mdl_user u ON u.id = ra.userid
+//     JOIN mdl_role r ON r.id = ra.roleid
+//     JOIN mdl_context ctx ON ctx.id = ra.contextid
+//     WHERE u.id=?
+//     GROUP BY u.id
+//     ORDER BY u.username ";
 
-    $result=$DB->get_record_sql($query,[$uid]);
+//     $result=$DB->get_record_sql($query,[$uid]);
 
-    return $result;
-}
+//     return $result;
+// }
 
 echo $OUTPUT->header();
 
