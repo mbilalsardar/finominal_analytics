a1f8ebb74d9124d4b304030f3caa95b3833c2d01
bilal <bilal@3ilogic.com>
2022-06-21 17:57:46 +0500

    fixing learner dashboard

--- corelibs/ajax.php
+++ corelibs/ajax.php
@@ -216,8 +216,7 @@ if($_POST['function'] == 'individual_dash_view') {
     $response['userinfo'] = $userinfo;
 
 
-    echo json_encode($response);
-    exit();
+  
 
     // Marks Overview.  
     $quiz_marks = [];
@@ -246,7 +245,10 @@ if($_POST['function'] == 'individual_dash_view') {
     $response['quiz_marks'] = $quiz_marks;
     $response['quiz_certificate'] = $certificate;
 
+    echo json_encode($response);
+    exit();
 
+    
     // Section wise marks overview
 
     $quizsections = course_quiz_sections($cid,$qid);
