c2d2c49ef1d7d3e2dabfcbf49fe804dfec2a9517
bilal <bilal@3ilogic.com>
2022-06-22 13:16:26 +0500

    fixing team averages chart

--- assets/js/dashboards/individual_dash.js
+++ assets/js/dashboards/individual_dash.js
@@ -261,8 +261,8 @@ $(document).ready(function () {
             },
             series: [],            
             xaxis: {
-                // categories : ['A','B','C']
-                type: 'categories',
+                categories : ['A','B','C']
+                // type: 'categories',
                 // categories : []
             },
         };
@@ -567,9 +567,9 @@ $(document).ready(function () {
 
             // Individual Quiz Section comparison chart
             individual_barChart.updateOptions({
-                xaxis: {
-                    categories : data['indi_team_averages_lable'],
-                },
+                // xaxis: {
+                //     categories : data['indi_team_averages_lable'],
+                // },
                 series: data['indi_team_averages_series'],
             });
 
