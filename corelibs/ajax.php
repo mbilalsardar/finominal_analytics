f94f9e8542d2557e0a235479e1d0b94e04f8fd60
bilal <bilal@3ilogic.com>
2022-06-22 13:29:53 +0500

    fixing team averages chart

--- corelibs/ajax.php
+++ corelibs/ajax.php
@@ -423,20 +423,21 @@ if($_POST['function'] == 'individual_dash_view') {
     //     ]
     // ];
 
-    $final_indivual_section_series = [ 
-        [ 
-            "name" =>"Team",
-            "data" => [10,20,30]
-        ],
-        [
-            "name" => "User",
-            "data" => [10,20,60]
-        ]
-    ];
+    // $final_indivual_section_series = [ 
+    //     [ 
+    //         "name" =>"Team",
+    //         "data" => [10,20,30]
+    //     ],
+    //     [
+    //         "name" => "User",
+    //         "data" => [10,20,60]
+    //     ]
+    // ];
 
 
-    $response['indi_team_averages_label'] = $final_indivual_section_labels;
-    $response['indi_team_averages_series'] = $final_indivual_section_series;
+    // $response['indi_team_averages_label'] = $final_indivual_section_labels;
+    // $response['indi_team_averages_series'] = $final_indivual_section_series;
+    $response['test'] = $section_users_precentage;
     
     
     echo json_encode($response);
