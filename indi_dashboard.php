c037ed3960228502a6baf665293e730e5e92fb66
bilal <bilal@3ilogic.com>
2023-02-14 18:03:41 +0500

    chart updates

--- indi_dashboard.php
+++ indi_dashboard.php
@@ -244,7 +244,7 @@ echo <<<HTML
                     <div class="card user-card bg-light-primary" style="min-height:320px;">
                             
                         <div class="card-header d-flex justify-content-between align-items-center mb-2">
-                                <span> <i data-feather="user" 
+                                <!-- <span> <i data-feather="user" 
                                     style='
                                     width: 50px; 
                                     height: 50px;
@@ -256,8 +256,18 @@ echo <<<HTML
                                     /* float : left; */
                                     '>
                                     </i>
-                                </span>
-                            <h4 class="card-title">Employee Profile</h4>
+                                </span> -->
+                            <h4 class="card-title"><i data-feather="user" 
+                                    style='
+                                    width: 50px; 
+                                    height: 50px;
+                                    background:#fff; 
+                                    color:#000;
+                                    border-radius:100px;
+                                    padding : 12px;
+                                   
+                                    '>
+                                    </i> Employee Profile</h4>
                         </div>
 
                         <div class="card-body">
