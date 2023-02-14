6bbde4287657c70943d94d5330ebbd39a0c59528
bilal <bilal@3ilogic.com>
2023-02-14 12:01:25 +0500

    user profile pic

--- indi_dashboard.php
+++ indi_dashboard.php
@@ -256,13 +256,13 @@ echo <<<HTML
                                     <div class="d-flex flex-column ml-1">
                                         <div class="user-info mb-1">
                                             <h4 class="mb-0" id='userinfo_username'>Username</h4>
-                                            <span class="float-right pr-2" id='userinfo_designation'>Designation</span>
+                                            <span class="" id='userinfo_designation'>Designation</span>
                                         </div>
                                         <div class="">
-                                            <h6>Team  : <span class="float-right pr-2" id='userinfo_team'>-</span></h6>
-                                            <h6>Location  :   <span class="float-right pr-2" id='userinfo_location'>-</span></h6>
-                                            <h6>Department  :  <span class="float-right pr-2" id='userinfo_department'>-</span></h6>
-                                            <h6>Manager  : <span class="float-right pr-2" id='userinfo_manager'>-</span></h6>
+                                            <h6>Team : <span class="" id='userinfo_team'>-</span></h6>
+                                            <h6>Location :   <span class="" id='userinfo_location'>-</span></h6>
+                                            <h6>Department :<span class="" id='userinfo_department'>-</span></h6>
+                                            <h6>Manager : <span class="" id='userinfo_manager'>-</span></h6>
                                         </div>
                                     </div>
                                 </div>
